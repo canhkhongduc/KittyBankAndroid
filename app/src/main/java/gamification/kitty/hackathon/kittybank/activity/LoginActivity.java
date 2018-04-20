de9b4220c5d2ba533abceeb2f9d3dc84fea6d408
minhnq97 <minhh.nguyen.97@gmail.com>
2018-04-21 02:50:37 +0700

    commit

--- app/src/main/java/gamification/kitty/hackathon/kittybank/activity/LoginActivity.java
+++ app/src/main/java/gamification/kitty/hackathon/kittybank/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package gamification.kitty.hackathon.kittybank.login;
+package gamification.kitty.hackathon.kittybank.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
