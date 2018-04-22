611aa7f6ba63c2e8d062ddff32ef8031057565e5
minhnq97 <minhh.nguyen.97@gmail.com>
2018-04-22 08:46:03 +0700

    commit hybrid UI

---
+++ app/src/main/java/gamification/kitty/hackathon/kittybank/activity/HybridActivity.java
@@ -0,0 +1,13 @@
+package gamification.kitty.hackathon.kittybank.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HybridActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hybrid);
+    }
+}
